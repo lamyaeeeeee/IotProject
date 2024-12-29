4e1e695b7651df570e5e1c3448418536683f77e2
lamyae OULAD ALI <154270262+lamyaeeeeee@users.noreply.github.com>
2024-12-29 14:41:23 +0100

    Update urls.py

--- projet/urls.py
+++ projet/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from DHT import views
-
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', include('DHT.urls')),
     path('download_csv/', views.download_csv, name='download_csv'),
     path('index/', views.table, name='table'),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 
 ]
