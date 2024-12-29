9c4c13de42c59119f84a7fff64182e3722e87caa
lamyae OULAD ALI <154270262+lamyaeeeeee@users.noreply.github.com>
2024-12-29 21:34:13 +0100

    Update settings.py

--- projet/settings.py
+++ projet/settings.py
@@ -23,7 +23,7 @@ EMAIL_HOST_PASSWORD = 'tidz whdr ybss dyju'
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
 
