db2f4e05b7072f7b45675cc298d1f45d5a7ccb94
lamyae OULAD ALI <154270262+lamyaeeeeee@users.noreply.github.com>
2024-12-29 17:02:17 +0100

    Update settings.py

--- projet/settings.py
+++ projet/settings.py
@@ -17,9 +17,9 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True  # ou False, selon la configuration de votrefournisseur de messagerie
 EMAIL_HOST_USER = 'lamyaeouladali@gmail.com'
 EMAIL_HOST_PASSWORD = 'tidz whdr ybss dyju'
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
+#CELERY_BROKER_URL = 'redis://localhost:6379/0'
+#CELERY_ACCEPT_CONTENT = ['json']
+#CELERY_TASK_SERIALIZER = 'json'
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
